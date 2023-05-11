c4b189ab7e1f03db955c4578c8ce845060c4b146
Chance Vodnoy <chancevodnoy129@gmail.com>
2023-05-10 18:28:59 -0700

    Triangle appears on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,149 @@
-#define GL_GLEXT_PROTOTYPES
-#include <SDL.h>
-#include <SDL_opengl.h>
+#include <iostream>
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 int main()
 {
-    /*
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window *window = SDL_CreateWindow("OpenGL Test",
-      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-    SDL_GLContext context = SDL_GL_CreateContext(window);
-
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
         return 1;
     }
+
+    // Set OpenGL attributes
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    // Create an SDL window with an OpenGL context
+    SDL_Window* window = SDL_CreateWindow("OpenGL Test",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        std::cerr << "SDL window creation failed: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create an OpenGL context
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (context == NULL) {
+        std::cerr << "OpenGL context creation failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Initialize GLEW to access modern OpenGL functions
+    glewExperimental = GL_TRUE;
+    GLenum glewInitResult = glewInit();
+    if (glewInitResult != GLEW_OK) {
+        std::cerr << "GLEW initialization failed: " << glewGetErrorString(glewInitResult) << std::endl;
+        SDL_GL_DeleteContext(context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create vertex buffer object for triangle position and color data
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    };
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create vertex array object that specifies how to interpret the vertex data
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+    // Create and compile shader program from vertex and fragment shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertexShaderSource = R"VERTEX(
+        #version 410 core
+        layout (location = 0) in vec3 aPos;
+        layout (location = 1) in vec3 aColor;
+        out vec3 fColor;
+        void main() {
+            gl_Position = vec4(aPos, 1.0);
+            fColor = aColor;
+        }
+    )VERTEX";
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    GLint vertexShaderCompilationResult;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderCompilationResult);
+    if (vertexShaderCompilationResult != GL_TRUE) {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "Vertex shader compilation failed: " << infoLog << std::endl;
+        glDeleteShader(vertexShader);
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+        SDL_GL_DeleteContext(context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragmentShaderSource = R"FRAGMENT(
+        #version 410 core
+        in vec3 fColor;
+        out vec4 FragColor;
+        void main() {
+            FragColor = vec4(fColor, 1.0);
+        }
+    )FRAGMENT";
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    GLint fragmentShaderCompilationResult;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentShaderCompilationResult);
+    if (fragmentShaderCompilationResult != GL_TRUE) {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr << "Fragment shader compilation failed: " << infoLog << std::endl;
+        glDeleteShader(fragmentShader);
+        glDeleteShader(vertexShader);
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+        SDL_GL_DeleteContext(context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    GLint shaderProgramLinkResult;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &shaderProgramLinkResult);
+    if (shaderProgramLinkResult != GL_TRUE) {
+        GLchar infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "Shader program link failed: " << infoLog << std::endl;
+        glDeleteProgram(shaderProgram);
+        glDeleteShader(fragmentShader);
+        glDeleteShader(vertexShader);
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+        SDL_GL_DeleteContext(context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    glUseProgram(shaderProgram);
 
     bool is_running = true;
     SDL_Event event;
@@ -29,25 +153,28 @@ int main()
                 is_running = false;
             }
         }
+
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw the triangle
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        SDL_GL_SwapWindow(window);
         SDL_Delay(16);
     }
 
+    // Clean up resources
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+    SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
-
     SDL_Quit();
-    */
 
-    int numberOfDrivers = SDL_GetNumVideoDrivers();
-    printf("Number of drivers: %d\n", numberOfDrivers);
-
-    for (int i = 0; i < numberOfDrivers; i++)
-        printf("Driver name: %s\n", SDL_GetVideoDriver(i));
-
-    int numberOfDisplays = SDL_GetNumVideoDisplays();
-    printf("Number of displays: %d\n", numberOfDisplays);
-
-    printf("Display 0 mode: %d\n", SDL_GetNumDisplayModes(0));
-
-    if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0)
-        SDL_Log("SDL fails to initialize video subsystem!\n%s", SDL_GetError());
+    return 0;
 }
